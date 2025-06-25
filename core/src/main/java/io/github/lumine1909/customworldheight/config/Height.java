78a5df629d0de8ca851e0befd1a274e715f85c1f
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-06-24 19:33:44 -0700

    Clean Up

--- core/src/main/java/io/github/lumine1909/customworldheight/config/Height.java
+++ core/src/main/java/io/github/lumine1909/customworldheight/config/Height.java
@@ -2,7 +2,6 @@ package io.github.lumine1909.customworldheight.config;
 
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public record Height(int height, int minY, int logicalHeight, Function<Optional<Integer>, Optional<Integer>> couldHeightFunc) {
 
