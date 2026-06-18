d93c153e6fdd14f7df5b6e8cb9427cbf03b18194
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-06-17 17:49:04 -0700

    [ci skip] Update modrinth version

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ modrinth {
 
     gameVersions.addAll(generateVersions("1.20", 5, 6))
     gameVersions.addAll(generateVersions("1.21", 0, 11))
-    gameVersions.addAll(generateVersions("26.1", 0, 1))
+    gameVersions.addAll(generateVersions("26.1", 0, 2))
     gameVersions.addAll(generateVersions("26.2", 0, 0))
 }
 
